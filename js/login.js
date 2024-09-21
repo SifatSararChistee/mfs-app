4cdcebe72e1cb2d5bfb31a4fe2bf3fe62396323d
SifatSararChistee <sifatsararchistee@gamil.com>
2024-09-21 15:38:36 +0600

    added home

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document
     const phoneNumber = document.getElementById("phone-number").value;
     const pinNumber = document.getElementById("pin-number").value;
     if (phoneNumber === "12346789" && pinNumber === "1234") {
-      window.location.href = "/home.html";
+      window.location.href = "./home.html";
     } else {
       alert("Incorrect PhoneNumber Or Pin");
     }
