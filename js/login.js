1f04d4bb485cb009216b82f2d9ebff6ce48da6a0
SifatSararChistee <sifatsararchistee@gamil.com>
2024-09-21 12:51:07 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    if (phoneNumber === "12346789" && pinNumber === "1234") {
+      window.location.href = "/home.html";
+    } else {
+      alert("Incorrect PhoneNumber Or Pin");
+    }
+  });
